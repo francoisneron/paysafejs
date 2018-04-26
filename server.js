d9e427bdf2633b50e5bbce959ce37aa7cc24152d
François Néron <francois.n3ron@gmail.com>
2018-04-25 21:02:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-var express = require('express')
-var app = express()
+var express = require("express");
+var bodyParser = require('body-parser');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('json spaces', 2);
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.set('port', (process.env.PORT || 8080))
+
+//the view, client key is passed to ejs template
+app.get("/", function (req, res) {
+  res.render("index.html");
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
