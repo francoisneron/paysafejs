3d8472913f31a7886e7001b290a54839e1933df4
François Néron <francois.n3ron@gmail.com>
2018-04-25 21:27:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var app = express();
 
+var paysafeKeys = require('./config');
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set('json spaces', 2);
 
-app.set('port', (process.env.PORT || 8080))
+//app.set('port', (process.env.PORT || 8080))
 
 //the view, client key is passed to ejs template
 app.get("/", function (req, res) {
-  res.render("index.html");
+  res.sendFile("index.html");
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+//app.listen(app.get('port'), function() {
+//  console.log("Node app is running at localhost:" + app.get('port'))
+//})
+
+app.listen(8080);
